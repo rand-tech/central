a39580e01c22d696eb6fb563ee7a1b277b550ab0
random <54098069+rand-tech@users.noreply.github.com>
2022-12-24 02:10:17 +0900

    add favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
+import './scss/styles.scss';
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,3 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
